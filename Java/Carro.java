26e74a5c24ae120d15d96d8333bfdf8716c00530
Renata300 <69905742+Renata300@users.noreply.github.com>
2021-04-20 13:13:18 -0300

    Iniciando codigo em Java

--- Java/Carro.java
+++ Java/Carro.java
@@ -1,10 +1,13 @@
+/* Esse é um projeto implementado em java referente à POO
+   O mesmo projeto consite de duas classes (App, que é a classe principal, e Carro) */
+   
 public class Carro {
     private String nome;
 
     public Carro (String nome){
         this.nome = nome;
     }
-
+ 
 	public String getNome() {
 		return nome.toLowerCase();
 	}

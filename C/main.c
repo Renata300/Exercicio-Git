1820f6830187e83465e69d9e9c06c1fa1fde11cb
Renata300 <69905742+Renata300@users.noreply.github.com>
2021-04-20 13:11:17 -0300

    Iniciando codigo em C

--- C/main.c
+++ C/main.c
@@ -1,86 +1,33 @@
-#include <stdio.h>
-#include <conio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int i = 0, j = 0;
-
-struct cadastro_cliente
-{
-    char nome[40];
-};
-
-struct cadastro_cliente Cliente[100];
-
-struct cadastro_carro
-{
-    char modelo[20];
-};
-
-struct cadastro_carro Carro[100];
-
-int cadastro_de_clientes(void);
-int lista_de_clientes(void);
-int cadastro_de_carros(void);
-int mostra_clientes(void);
-
-int cadastro_de_clientes(void)
-{
-    printf("Cadastro %d de 100\n", j);
-    if (j < 100)
-    {
-        printf("Digite o nome do Cliente: ");
-        gets(Cliente[j].nome);
-        j++;
-    }
-    return (0);
-}
-
-int lista_de_clientes(void)
-{
-    int k = 0;
-    
-    if (Cliente[k] != 0)
-    {
-        k++;
-        printf("\nNome do Cliente: %s\n", Cliente[k].nome);
-        getch();
-    }
-    return (0);
-}
-
-int cadastro_de_carros(void)
-{  
-    printf("Cadastro de carro %d de 100\n", i);
-    if (i < 100)
-    {
-        printf("Digite o modelo do carro: ");
-        gets(Carro[i].modelo);
-        i++;
-    }
-    return (0);
-}
-
-int mostra_clientes(void)
-{
-    for (j = 0; j < 100; j++)
-    {
-        printf("Cliente: %c", Cliente[i].nome);
-    }
-    return (0);
-}
-
-int main(void)
-{
-    cadastro_de_clientes();
-    printf("\n");
-
-    lista_de_clientes();
-    printf("\n");
-
-    cadastro_de_carros();
-    printf("\n");
-
-    mostra_clientes();
-    printf("\n");
-}
+/* Esse código consiste da implementação de aluguel de carros através da linguagem C */
+ 
+#include <stdio.h> 
+#include <conio.h> 
+#include <string.h> 
+#include <stdlib.h> 
+ 
+int i = 0, j = 0; 
+ 
+struct lista_carro 
+{ 
+    char modelo[20]; 
+}; 
+ 
+struct lista_carro Carro[100]; 
+  
+int lista_de_carros(void) 
+{   
+    printf("Cadastro de carro %d dos carros\n", i); 
+    if (i < 100) 
+    { 
+        printf("Digite o modelo do carro: "); 
+        gets(Carro[i].modelo); 
+        i++; 
+    } 
+    return (0); 
+} 
+ 
+int main(void) 
+{ 
+    lista_de_carros(); 
+    printf("\n"); 
+} 

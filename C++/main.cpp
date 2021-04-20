9f9a0462f59e118d1bed8a902b063eeb25bb0b55
Renata300 <69905742+Renata300@users.noreply.github.com>
2021-04-20 17:32:22 -0300

    Iniciando codigo em C++

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,71 +1,49 @@
 /* Esse código consiste da implementação de aluguel de carros através da linguagem C++ */
 
-#include <iterator> 
-#include <cctype> 
-#include <string> 
-#include <iostream> 
-#include <vector> 
- 
-using namespace std; 
- 
-class Carro 
-{ 
-private: 
-    string nome; 
- 
-public: 
-    Carro(string); 
-    Carro(); // construtor vazio para poder criar um vetor 
-    ~Carro(); 
-    void setNome(string); 
-    string getNome(); 
-}; 
- 
-Carro::Carro(string nome) 
-{ 
-    this->nome = nome; 
-} 
- 
-Carro::Carro() {} 
-Carro::~Carro() {} 
- 
-void Carro::setNome(string nome) 
-{ 
-    this->nome = nome; 
-} 
- 
-string Carro::getNome() 
-{ 
-    return nome; 
-} 
- 
-int main() 
-{ 
-    int n; 
-    string nome; 
-    vector<Carro> Carros; 
-    cout << "Digite quantos carros você quer adicionar ao catálogo"; 
-    cout << '\n'; 
-    cin >> n; 
-    string nomes[n]; 
-    string *p = nomes; 
- 
-    //Carro *carros=new Carro[n]; 
-    for (int i = 0; i < n; i++) 
-    { 
-        cout << "Digite o nome do carro a ser adicionado ao catálogo"; 
-        cout << '\n'; 
-        cin >> nome; 
-        Carro c(nome); 
-        Carros.push_back(c); 
-        nomes[i] = c.getNome(); 
-    } 
-    cout << '\n'; 
-    cout << "------Lista de Carros: ------"; 
-    cout << '\n'; 
-    for (int i = 0; i < n; i++) 
-    { 
-        cout << nomes[i]; 
-        cout << '\n'; 
-    } 
-} 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Carro {
+	private:
+		string nome;
+	public:
+		Carro(string);
+		Carro();
+		~Carro();
+		void setNome (string);
+		string getNome();
+};
+Carro::Carro(string nome)
+{
+	this->nome=nome;
+}
+Carro::Carro(){}
+Carro::~Carro(){}
+void Carro::setNome (string nome){
+	this->nome=nome;
+}
+string Carro::getNome (){
+	return nome;
+}
+int main () {
+	int n;
+	string nome;
+cout << "Digite quantos carros você quer adicionar ao catálogo";
+cout << '\n';
+cin >> n;
+Carro Carros[n];
+for (int i=0; i<n; i++){
+	cout << "Digite o nome do carro a ser adicionado ao catálogo";
+	cout << '\n';
+	cin >> nome;
+	Carros[i].setNome(nome);
+}
+cout <<'\n';
+cout << "------Lista de Carros: ------";
+cout <<'\n';
+for (int i=0; i<n;i++){
+	cout << Carros[i].getNome();
+	cout << '\n';
+}
+}
